606b1160723b66f983e48b6c3700282b3b2ca4ea
Jazyac <jazy_ac@hotmail.com>
2017-10-31 01:34:59 +1300

    Added functionality for user entered subreddit

--- CreateThreads.js
+++ CreateThreads.js
@@ -1,8 +1,36 @@
 /**
  * Using:http://api.jquery.com/jquery.getjson/
  */
-
-
+var url="";
+function submitFunct() {
+ document.getElementById("features").innerHTML ="";
+ var field=document.getElementById("subredditField");
+ var nameOfSubreddit=field.value;
+ url="http://www.reddit.com/r/"+nameOfSubreddit+"/top.json?jsonp=?";
+ 
+ $.getJSON(
+			url,
+			function foo(data)
+			{
+				$.each(
+						data.data.children.slice(0, 6),
+						function (i, thread) {
+							$("#features").append( 
+									'<section>'+
+									'<a href='+thread.data.url+' target="_blank"><span class="icon major fa-desktop"></span></a>'+
+									'<h3 >'+thread.data.title+'</h3>'+
+									'</section>'
+							);
+							console.log(thread.data.url);
+						}
+				)
+			}
+	)
+ 
+ document.getElementById("VISIT").href="https://www.reddit.com/r/"+nameOfSubreddit+"/top/"; 
+ document.getElementById("VISIT").innerHTML="Visit r/"+nameOfSubreddit;
+}
+ 
 $(document).ready( function() {
 
 	$.getJSON(
@@ -12,7 +40,7 @@ $(document).ready( function() {
 				$.each(
 						data.data.children.slice(0, 6),
 						function (i, thread) {
-							$("#features").append( 
+							$("#featuresNews").append( 
 									'<section>'+
 									'<a href='+thread.data.url+' target="_blank"><span class="icon major fa-desktop"></span></a>'+
 									'<h3 >'+thread.data.title+'</h3>'+
